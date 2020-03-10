e81b428628558373c45eb91229143de35c26d39e
idk <zhouxugang000106@gmail.com>
2020-03-10 16:41:57 +0800

    循环有误

--- subway/src/dataProcess/GoFunc.java
+++ subway/src/dataProcess/GoFunc.java
@@ -22,13 +22,14 @@ public class GoFunc {
     //存放距离
     private Integer[][] arr1;
     //存放线路
-    private List[][] arr2;
+    // arr2[i][j] 是一个存放Integer的list
+    private List<Integer>[][] arr2;
 
     private Map<Integer, String> station = new HashMap<>();
     private Map<String, List<Integer>> route = new HashMap<>();
     private List<String> routeNameList;
     private Integer n;
-    private Integer max = Integer.MAX_VALUE;
+    private Integer max = 1000;
 
     public GoFunc() throws IOException {
         routeNameList = preProcess("static/station.txt", station, "static/route.txt", route);
@@ -36,6 +37,7 @@ public class GoFunc {
         arr1 = new Integer[n + 1][n + 1];
         arr2 = new ArrayList[n + 1][n + 1];
         initArr();
+        process();
     }
 
     public static void main(String[] args) throws IOException {
@@ -74,15 +76,90 @@ public class GoFunc {
 
         //初始化arr2
         //arr2中的list放起始点
-        for (int i = 1; i <= n; i++){
-            for (int j = 1; j <= n; j++){
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
                 arr2[i][j] = new ArrayList();
                 arr2[i][j].add(i);
+                arr2[i][j].add(j);
             }
         }
 
     }
 
+    public void process() {
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                for (int k = 1; k <= n; k++) {
+                    int distance = arr1[i][k] + arr1[k][j];
+
+                    List<Integer> x = new ArrayList<>();
+                    x.addAll(arr2[i][k]);
+                    List<Integer> y = new ArrayList<>();
+                    y.addAll(arr2[k][j]);
+                    // x y 为完整线路 即包括起始站和终点站
+//                    x.add(k);
+//                    y.add(j);
+                    //判断是否换乘
+                    Integer a = x.get(x.size() - 2);
+                    Integer b = x.get(x.size() - 1);
+                    Integer c = y.get(0);
+                    boolean flag = Switch(a, b, c);
+                    if (flag)
+                        distance++;
+
+                    if (distance < arr1[i][j]) {
+                        arr1[i][j] = distance;
+                        List<Integer> list = new ArrayList<>();
+                        list.addAll(arr2[i][k]);
+                        list.remove(list.size()-1);
+                        list.addAll(arr2[k][j]);
+                        arr2[i][j].clear();
+                        arr2[i][j].addAll(list);
+                    }
+                }
+//                System.out.print(i + " " + j + " ");
+//                System.out.println(arr2[i][j]);
+            }
+            // System.out.println(i);
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+
+                System.out.print(arr1[i][j] + "  ");
+                for (int k = 0; k < arr2[i][j].size(); k++) {
+                    System.out.print(arr2[i][j].get(k) + "-");
+                }
+                System.out.println();
+            }
+        }
+    }
+
+    //判断是否换乘
+    public boolean Switch(Integer a, Integer b, Integer c) {
+
+        String route1 = null;
+        String route2 = null;
+
+        for (String routeName : routeNameList) {
+            List<Integer> list = route.get(routeName);
+            for (int i = 0; i < list.size() - 1; i++) {
+                if (list.get(i).equals(a) && list.get(i + 1).equals(b)) {
+                    route1 = routeName;
+                } else if (list.get(i).equals(b) && list.get(i + 1).equals(a)) {
+                    route1 = routeName;
+                } else if (list.get(i).equals(b) && list.get(i + 1).equals(c)) {
+                    route2 = routeName;
+                } else if (list.get(i).equals(c) && list.get(i + 1).equals(b)) {
+                    route2 = routeName;
+                }
+            }
+            if (route1 != null && route2 != null)
+                break;
+        }
 
 
+        return route1 != null && route1.equals(route2);
+    }
 }
